507fbe78a826935cfa1e7521bebe058571e39626
majakwani <anasjakwani72@gmail.com>
2022-09-14 15:17:42 +0500

    Re-wrote script to render questions and answers correctly

--- quizzical/components/Question.jsx
+++ quizzical/components/Question.jsx
@@ -2,32 +2,32 @@ import React from "react";
 import { nanoid } from "nanoid";
 
 export default function Question(props) {
-  let count = 0
-  const buttons = props.data.answers.map((answer) => {
-    const styles = {
-        backgroundColor: answer === props.answerData[count].value ? "#D6DBF5" : "transparent"
-    }
-    console.log(props.answerData[count], answer)
-    count += 1
-    return (
+  let count = props.questionData.index
+  const styles = {
+    backgroundColor: "",
+    color: "yellow"
+  };
+  const buttons = props.questionData.answers.map((answer) => {
+    styles.backgroundColor = answer === props.answerData[count].value ? 
+    "#D6DBF4" :
+    "transparent"
+      return (
         <button
-        key={nanoid()}
         type="button"
-        onClick={() => props.clickEvent(props.data.id, answer)}
-        style = {styles}
+        style={styles}
+        key={nanoid()}
+        onClick={() => props.clickEvent(props.questionData.id, answer)}
         >
         {answer}
       </button>
     );
   });
+  count += 1
   return (
     <div className="question">
-      <p>{props.data.question}</p>
-      <div className="choiceContainer">
-        {buttons}
-      </div>
+      <p>{props.questionData.question}</p>
+      <div className="choiceContainer">{buttons}</div>
       <div className="line"></div>
-    </div>
+      </div>
   );
-
 }
